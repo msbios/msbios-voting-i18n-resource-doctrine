5a37b5e0fb498541e8fdf1a7dabdd5726896314f
Judzhin Miles <info@msbios.com>
2018-05-08 16:18:29 +0300

    Lazy Composer Commiter

--- config/application.config.php
+++ config/application.config.php
@@ -6,6 +6,7 @@
 return [
     // Retrieve list of modules used in this application.
     'modules' => [
+        'MSBios\I18n\Doctrine',
         'Zend\Paginator',
         'Zend\Mvc\Console',
         'Zend\Serializer',
--- src/Module.php
+++ src/Module.php
@@ -16,7 +16,7 @@ use Zend\Loader\StandardAutoloader;
 class Module implements ModuleInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2';
 
     /**
      * Returns configuration to merge with application configuration
